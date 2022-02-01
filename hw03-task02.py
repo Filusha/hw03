d0d738d7361031edf1db82b621218ccb8d27ff76
Filusha <56093754+Filusha@users.noreply.github.com>
2022-02-02 01:56:05 +0300

    Update hw03-task02.py

--- hw03-task02.py
+++ hw03-task02.py
@@ -9,7 +9,5 @@
 def fn_data_processing(ps_name, ps_surname, pd_birthday, ps_city, ps_email, ps_phone):
     return ps_name + " " + ps_surname + ", " + pd_birthday + ", " + ps_city + ", " + ps_email + ", " + ps_phone
 
-
-
 vs_ClientInfo = fn_data_processing(ps_name="Василий", ps_surname="Пупыркин", pd_birthday="01.01.1991", ps_city="Миндюкино", ps_email="VPupirkin@GMail.com", ps_phone="+7(999)111-2233")
 print("Client information: ", vs_ClientInfo)
